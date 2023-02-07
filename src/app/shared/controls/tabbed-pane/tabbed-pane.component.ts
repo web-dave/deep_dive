d9f8bdd52139241135307ee7cc172f828e3c9e70
web-dave <dave@webdave.de>
2023-02-07 14:20:44 +0100

    Content and View Referencing the Parent Component

--- src/app/shared/controls/tabbed-pane/tabbed-pane.component.ts
+++ src/app/shared/controls/tabbed-pane/tabbed-pane.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { AfterContentInit, Component } from '@angular/core';
+import { TabComponent } from '../tab/tab.component';
 
 @Component({
   selector: 'app-tabbed-pane',
   templateUrl: './tabbed-pane.component.html',
   styleUrls: ['./tabbed-pane.component.scss']
 })
-export class TabbedPaneComponent {
+export class TabbedPaneComponent implements AfterContentInit {
+  tabs: TabComponent[] = [];
+  activeTab: TabComponent | undefined;
 
+  ngAfterContentInit() {
+    if (this.tabs.length > 0) {
+      this.activate(this.tabs[0]);
+    }
+  }
+  activate(active: TabComponent) {
+    this.tabs.forEach((tab) => {
+      tab.visible = tab === active;
+    });
+  }
+
+  register(tab: TabComponent) {
+    this.tabs.push(tab);
+  }
 }
