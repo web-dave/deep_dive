b45723a57f55de7aec7ba2ccc92e83e7a1c5c358
web-dave <dave@webdave.de>
2023-02-08 16:37:30 +0100

    Aux Routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@
 
 import { Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
+import { BasketComponent } from './basket/basket.component';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
@@ -14,6 +15,11 @@ export const APP_ROUTES: Routes = [
   {
     path: 'home',
     component: HomeComponent
+  },
+  {
+    path: 'basket',
+    component: BasketComponent,
+    outlet: 'ratata'
   },
   {
     path: 'about',
